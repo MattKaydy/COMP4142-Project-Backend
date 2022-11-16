e69a47aaaf06029cc9113c43be2fcb7f43c96112
Matthew Kong <www.imksc@live.com>
2022-11-16 19:32:54 +0800

    package.json fixes

--- src/blockchain.js
+++ src/blockchain.js
@@ -2,7 +2,7 @@
 const crypto = require('crypto');
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
-const debug = require('debug')('savjeecoin:blockchain');
+const debug = require('debug')('COMP4142-Project-Backend:blockchain');
 
 class Transaction {
   /**
