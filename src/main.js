76f3742f74e9ea91b0afec95356136f079f2f54e
wongthomas1030 <wongthomas1030@gmail.com>
2022-11-20 11:36:48 +0800

    Added Dynamic difficulties, modified construct blockchian
    Commented Local storage in main.js

--- src/main.js
+++ src/main.js
@@ -25,14 +25,14 @@ console.log(myWalletAddress);
 
 // Get blockchain from storage and construct it
 const savjeeCoin = new Blockchain();
-const dataDir = './data';
-if (!fs.existsSync(dataDir)) {
-  fs.mkdirSync(dataDir);
-}
-const blockchainPath = './data/blockchain.json';
+//const dataDir = './data';
+//if (!fs.existsSync(dataDir)) {
+//  fs.mkdirSync(dataDir);
+//}
+/*const blockchainPath = './data/blockchain.json';
 if (fs.existsSync(blockchainPath)) {
   savjeeCoin.constructBlockchain(blockchainPath);
-}
+}*/
 
 // Mine first block
 savjeeCoin.minePendingTransactions(myWalletAddress);
@@ -53,6 +53,94 @@ savjeeCoin.addTransaction(tx2);
 // Mine block
 savjeeCoin.minePendingTransactions(myWalletAddress);
 
+// Create second transaction
+const tx3 = new Transaction(myWalletAddress, 'address1', 50);
+tx3.signTransaction(myKey);
+savjeeCoin.addTransaction(tx3);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx4 = new Transaction(myWalletAddress, 'address1', 50);
+tx4.signTransaction(myKey);
+savjeeCoin.addTransaction(tx4);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx5 = new Transaction(myWalletAddress, 'address1', 50);
+tx5.signTransaction(myKey);
+savjeeCoin.addTransaction(tx5);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx6 = new Transaction(myWalletAddress, 'address1', 50);
+tx6.signTransaction(myKey);
+savjeeCoin.addTransaction(tx6);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx7 = new Transaction(myWalletAddress, 'address1', 50);
+tx7.signTransaction(myKey);
+savjeeCoin.addTransaction(tx7);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx8 = new Transaction(myWalletAddress, 'address1', 50);
+tx8.signTransaction(myKey);
+savjeeCoin.addTransaction(tx8);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx9 = new Transaction(myWalletAddress, 'address1', 50);
+tx9.signTransaction(myKey);
+savjeeCoin.addTransaction(tx9);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx10 = new Transaction(myWalletAddress, 'address1', 50);
+tx10.signTransaction(myKey);
+savjeeCoin.addTransaction(tx10);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx11 = new Transaction(myWalletAddress, 'address1', 50);
+tx11.signTransaction(myKey);
+savjeeCoin.addTransaction(tx11);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx12 = new Transaction(myWalletAddress, 'address1', 50);
+tx12.signTransaction(myKey);
+savjeeCoin.addTransaction(tx12);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx13 = new Transaction(myWalletAddress, 'address1', 50);
+tx13.signTransaction(myKey);
+savjeeCoin.addTransaction(tx12);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
 console.log();
 console.log(savjeeCoin.chain);
 console.log();
@@ -68,5 +156,5 @@ console.log();
 console.log('Blockchain valid?', savjeeCoin.isChainValid() ? 'Yes' : 'No');
 
 // fs.writeFileSync('./data/blockchain.json', util.inspect(savjeeCoin), 'utf-8');
-const data = JSON.stringify(savjeeCoin);
-fs.writeFileSync('./data/blockchain.json', data);
+//const data = JSON.stringify(savjeeCoin);
+//fs.writeFileSync('./data/blockchain.json', data);
