076a77f4d42e0984fa494ea66b1ac16838353883
chan-anthony <92667885+chan-anthony@users.noreply.github.com>
2022-11-23 01:56:13 +0800

    [IMP] refactoed network and sendBlockRequest JS

--- src/sendBlockRequest.js
+++ src/sendBlockRequest.js
@@ -1,20 +1,14 @@
-const express = require("express");
-var router = express.Router();
+const request = require('request');
 
-var request = require('request');
-
-var requestData = { answer: 42 };
-
-var data = {
-    url: 'http://localhost:12345/ask',
-    json: true,
-    body: { answer: 42}
-}
+const data = {
+  url: 'http://localhost:12345/ask',
+  json: true,
+  body: { answer: 42 },
+};
 
 request.post(data, function (error, response, body) {
-        console.log("POST REQUEST Sent!")
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-        }
-    }
-);
+  console.log('POST REQUEST Sent!');
+  if (!error && response.statusCode === 200) {
+    console.log(body);
+  }
+});
